3cbc157f343b23443b941490939b51b64b3271fe
Romantic <dupyi0912@gmail.com>
2022-07-09 22:43:22 +0800

    feat: first commit sunofbeache-rn

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+  // 不使用缩进符，而使用空格
+  useTabs: false,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+};
